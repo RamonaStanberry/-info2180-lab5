8a2775eb995da530b6ebee933d93a861496fc0c4
Ramona Stanberry <monacstanberry@gmail.com>
2021-11-06 01:58:54 -0500

    Excercise 4 Complete- Table Displaying

--- world.php
+++ world.php
@@ -9,11 +9,24 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <br>
-<br>
-<hr>
-<ul>
+<table>
+  <caption>List of Countries </caption>
+  <thead>
+    <tr>
+    <th>Name</th>
+      <th>Continent</th>
+      <th>Independence</th>
+      <th>Head of State </th>
+    </tr>
+  </thead>  
+  <tbody>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-  <br>
+  <tr>
+    <td><?= $row['name']; ?></td>
+    <td><?= $row['continent']; ?></td>
+    <td><?= $row['independence_year']; ?></td>
+    <td><?= $row['head_of_state']; ?> </td>
+  </tr>
 <?php endforeach; ?>
-</ul>
+</tbody>
+</table>
